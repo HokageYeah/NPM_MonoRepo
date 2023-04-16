58886c2f21f2da20a0b14831d01be3a500c06bce
余晔 <18838280615@163.com>
2023-04-16 11:08:51 +0800

    提交

--- main/src/main.ts
+++ main/src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import { axiosGet } from 'common'
+import { axiosPost } from 'common'
 createApp(App).mount('#app')
